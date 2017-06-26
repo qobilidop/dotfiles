bf7e1db7e3bb71b7d162ad7f8e4fc72dde5f6eed
qobilidop <qobilidop@gmail.com>
2017-06-26 16:16:20 -0700

    Unify conda install scripts

---
+++ conda/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+function install()
+{
+    OS=$1
+    curl https://repo.continuum.io/miniconda/Miniconda3-latest-$OS-x86_64.sh -o ~/miniconda.sh
+    bash ~/miniconda.sh -b -p $HOME/miniconda
+    rm ~/miniconda.sh
+}
+
+case $(uname) in
+    "Darwin")
+        install "MacOSX"
+        ;;
+    "Linux")
+        install "Linux"
+        ;;
+    *)
+        echo "Unknown OS"
+        ;;
+esac
--- conda/install_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -o ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-rm ~/miniconda.sh
