605b2db00f7c450cf3516a1e3d111d3a50d83fa7
qobilidop <qobilidop@gmail.com>
2018-03-24 13:00:33 -0700

    Use the official install script for Linuxbrew

--- script/install-brew.sh
+++ script/install-brew.sh
@@ -2,16 +2,9 @@
 
 # Determine which brew to install
 if [[ $(uname) == 'Darwin' ]]; then
-    # For Homebrew, it's recommended to just use the install script.
-    # See https://brew.sh
+    # https://brew.sh
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    # For Linuxbrew, let's use the alternative installation method.
-    # See http://linuxbrew.sh/#alternative-installation
-    dir=~/.local/opt/linuxbrew
-    if [ ! -d $dir ]; then
-        git clone https://github.com/Linuxbrew/brew.git $dir
-    fi
-    cd ~/.local/bin
-    ln -fs ../opt/brew/bin/* .
+    # http://linuxbrew.sh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 fi
