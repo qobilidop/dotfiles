03a2efe3d3899cc1d3b842d7aceef8833baf613f
qobilidop <qobilidop@gmail.com>
2018-03-24 16:04:06 -0700

    Small tweaking

--- script/install-conda.sh
+++ script/install-conda.sh
@@ -3,8 +3,9 @@
 function install()
 {
     OS=$1
+    # https://conda.io/miniconda.html
     curl https://repo.continuum.io/miniconda/Miniconda3-latest-$OS-x86_64.sh -o miniconda.sh
-    curl https://repo.continuum.io/miniconda/Miniconda3-latest-$OS-x86_64.sh | bash miniconda.sh -b -p $HOME/.local/opt/conda
+    bash miniconda.sh -b -p $HOME/.local/opt/conda
     rm miniconda.sh
 }
 
