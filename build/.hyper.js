26102497b6b986915c2c97b392602e991feacb4e
Bili Dong <qobilidop@gmail.com>
2019-12-23 16:13:42 -0800

    Show the build dir
    Yes, it is duplication. But it's more intuitive to view and check. Since the files are small, it's worthing duplicating.

---
+++ build/.hyper.js
@@ -0,0 +1,17 @@
+// See https://hyper.is
+
+module.exports = {
+
+  updateChannel: 'canary',
+
+  // See https://hyper.is/#cfg
+  config: {
+    fontFamily: 'Fira Code',
+  },
+
+  // See https://hyper.is/#extensions
+  plugins: [
+    'hyper-solarized-dark'
+  ],
+
+};
